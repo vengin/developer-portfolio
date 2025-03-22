7b2c94920fc82e6728b09bc2f6128dac9d2d0208
vengin <gvengin@gmail.com>
2025-03-22 18:27:54 +0100

    Adding skills

--- src/utils/skillsImage.js
+++ src/utils/skillsImage.js
@@ -1,3 +1,17 @@
+import fpga from '../assets/svg/skills/fpga.png'
+import cpld from '../assets/svg/skills/cpld.png'
+import vhdl from '../assets/svg/skills/vhdl.png'
+import microprocessors from '../assets/svg/skills/microprocessors.jpg'
+import firmware from '../assets/svg/skills/firmware.jpg'
+import systemverilog from '../assets/svg/skills/systemverilog.jpg'
+import kria26 from '../assets/svg/skills/kria k26.jpg'
+import rpi from '../assets/svg/skills/Raspberry Pi.png'
+import labview from '../assets/svg/skills/labview.jpg'
+import usb from '../assets/svg/skills/USB.png'
+import pci from '../assets/svg/skills/PCI.png'
+import hdmi from '../assets/svg/skills/HDMI.png'
+import displayport from '../assets/svg/skills/DisplayPort.png'
+
 import gcp from '../assets/svg/skills/gcp.svg'
 import html from '../assets/svg/skills/html.svg'
 import photoshop from '../assets/svg/skills/photoshop.svg'
@@ -77,157 +91,188 @@ import canva from '../assets/svg/skills/canva.svg'
 
 
 export const skillsImage = (skill) => {
-    const skillID = skill.toLowerCase();
-    switch (skillID) {
-        case 'gcp':
-            return gcp;
-        case 'html':
-            return html;
-        case 'photoshop':
-            return photoshop;
-        case 'docker':
-            return docker;
-        case 'illustrator':
-            return illustrator;
-        case 'adobe xd':
-            return adobeXd;
-        case 'after effects':
-            return afterEffects;
-        case 'css':
-            return css;
-        case 'angular':
-            return angular;
-        case 'javascript':
-            return javascript;
-        case 'next js':
-            return nextJS;
-        case 'nuxt js':
-            return nuxtJS;
-        case 'react':
-            return react;
-        case 'svelte':
-            return svelte;
-        case 'typescript':
-            return typescript;
-        case 'vue':
-            return vue;
-        case 'bootstrap':
-            return bootstrap;
-        case 'bulma':
-            return bulma;
-        case 'capacitorjs':
-            return capacitorjs;
-        case 'coffeescript':
-            return coffeescript;
-        case 'memsql':
-            return memsql;
-        case 'mongodb':
-            return mongoDB;
-        case 'mysql':
-            return mysql;
-        case 'postgresql':
-            return postgresql;
-        case 'tailwind':
-            return tailwind;
-        case 'vitejs':
-            return vitejs;
-        case 'vuetifyjs':
-            return vuetifyjs;
-        case 'c':
-            return c;
-        case 'c++':
-            return cplusplus;
-        case 'c#':
-            return csharp;
-        case 'dart':
-            return dart;
-        case 'go':
-            return go;
-        case 'java':
-            return java;
-        case 'kotlin':
-            return kotlin;
-        case 'julia':
-            return julia;
-        case 'matlab':
-            return matlab;
-        case 'php':
-            return php;
-        case 'python':
-            return python;
-        case 'ruby':
-            return ruby;
-        case 'swift':
-            return swift;
-        case 'adobe audition':
-            return adobeaudition;
-        case 'aws':
-            return aws;
-        case 'deno':
-            return deno;
-        case 'django':
-            return django;
-        case 'firebase':
-            return firebase;
-        case 'gimp':
-            return gimp;
-        case 'git':
-            return git;
-        case 'graphql':
-            return graphql;
-        case 'lightroom':
-            return lightroom;
-        case 'materialui':
-            return materialui;
-        case 'nginx':
-            return nginx;
-        case 'numpy':
-            return numpy;
-        case 'opencv':
-            return opencv;
-        case 'premiere pro':
-            return premierepro;
-        case 'pytorch':
-            return pytorch;
-        case 'selenium':
-            return selenium;
-        case 'strapi':
-            return strapi;
-        case 'tensorflow':
-            return tensorflow;
-        case 'webix':
-            return webix;
-        case 'wordpress':
-            return wordpress;
-        case 'azure':
-            return azure;
-        case 'blender':
-            return blender;
-        case 'fastify':
-            return fastify;
-        case 'figma':
-            return figma;
-        case 'flutter':
-            return flutter;
-        case 'haxe':
-            return haxe;
-        case 'ionic':
-            return ionic;
-        case 'markdown':
-            return markdown;
-        case 'microsoft office':
-            return microsoftoffice;
-        case 'picsart':
-            return picsart;
-        case 'sketch':
-            return sketch;
-        case 'unity':
-            return unity;
-        case 'wolframalpha':
-            return wolframalpha;
-        case 'canva':
-            return canva;
-        default:
-            break;
-    }
+  const skillID = skill.toLowerCase();
+  switch (skillID) {
+    case 'fpga':
+      return fpga;
+    case 'cpld':
+      return cpld;
+    case 'vhdl':
+      return vhdl;
+    case 'microprocessors':
+      return microprocessors;
+    case 'firmware':
+      return firmware;
+    // case 'vhdl':
+    //   return vhdl;
+    case 'systemverilog':
+      return systemverilog;
+    case 'kria k26':
+      return kria26;
+    case 'raspberry pi':
+      return rpi;
+    case 'displayport':
+      return displayport;
+    case 'usb':
+      return usb;
+    case 'hdmi':
+      return hdmi;
+    case 'pci':
+      return pci;
+  
+    case 'labview':
+      return labview;
+    case 'matlab':
+      return matlab;
+    case 'c':
+      return c;
+    case 'c++':
+      return c++;
+    case 'python':
+      return python;
+    case 'git':
+      return git;
+    case 'docker':
+      return docker;
+
+    // case 'gcp':
+    //   return gcp;
+    // case 'html':
+    //   return html;
+    // case 'photoshop':
+    //   return photoshop;
+    // case 'illustrator':
+    //   return illustrator;
+    // case 'adobe xd':
+    //   return adobeXd;
+    // case 'after effects':
+    //   return afterEffects;
+    // case 'css':
+    //   return css;
+    // case 'angular':
+    //   return angular;
+    // case 'javascript':
+    //   return javascript;
+    // case 'next js':
+    //   return nextJS;
+    // case 'nuxt js':
+    //   return nuxtJS;
+    // case 'react':
+    //   return react;
+    // case 'svelte':
+    //   return svelte;
+    // case 'typescript':
+    //   return typescript;
+    // case 'vue':
+    //   return vue;
+    // case 'bootstrap':
+    //   return bootstrap;
+    // case 'bulma':
+    //   return bulma;
+    // case 'capacitorjs':
+    //   return capacitorjs;
+    // case 'coffeescript':
+    //   return coffeescript;
+    // case 'memsql':
+    //   return memsql;
+    // case 'mongodb':
+    //   return mongoDB;
+    // case 'mysql':
+    //   return mysql;
+    // case 'postgresql':
+    //   return postgresql;
+    // case 'tailwind':
+    //   return tailwind;
+    // case 'vitejs':
+    //   return vitejs;
+    // case 'vuetifyjs':
+    //   return vuetifyjs;
+    //   return cplusplus;
+    // case 'c#':
+    //   return csharp;
+    // case 'dart':
+    //   return dart;
+    // case 'go':
+    //   return go;
+    // case 'java':
+    //   return java;
+    // case 'kotlin':
+    //   return kotlin;
+    // case 'julia':
+    //   return julia;
+    //   case 'php':
+    //   return php;
+    // case 'ruby':
+    //   return ruby;
+    // case 'swift':
+    //   return swift;
+    // case 'adobe audition':
+    //   return adobeaudition;
+    // case 'aws':
+    //   return aws;
+    // case 'deno':
+    //   return deno;
+    // case 'django':
+    //   return django;
+    // case 'firebase':
+    //   return firebase;
+    // case 'gimp':
+    //   return gimp;
+    // case 'graphql':
+    //   return graphql;
+    // case 'lightroom':
+    //   return lightroom;
+    // case 'materialui':
+    //   return materialui;
+    // case 'nginx':
+    //   return nginx;
+    // case 'numpy':
+    //   return numpy;
+    // case 'opencv':
+    //   return opencv;
+    // case 'premiere pro':
+    //   return premierepro;
+    // case 'pytorch':
+    //   return pytorch;
+    // case 'selenium':
+    //   return selenium;
+    // case 'strapi':
+    //   return strapi;
+    // case 'tensorflow':
+    //   return tensorflow;
+    // case 'webix':
+    //   return webix;
+    // case 'wordpress':
+    //   return wordpress;
+    // case 'azure':
+    //   return azure;
+    // case 'blender':
+    //   return blender;
+    // case 'fastify':
+    //   return fastify;
+    // case 'figma':
+    //   return figma;
+    // case 'flutter':
+    //   return flutter;
+    // case 'haxe':
+    //   return haxe;
+    // case 'ionic':
+    //   return ionic;
+    // case 'markdown':
+    //   return markdown;
+    // case 'microsoft office':
+    //   return microsoftoffice;
+    // case 'picsart':
+    //   return picsart;
+    // case 'sketch':
+    //   return sketch;
+    // case 'unity':
+    //   return unity;
+    // case 'wolframalpha':
+    //   return wolframalpha;
+    // case 'canva':
+    //   return canva;
+    default:
+      break;
+  }
 }
