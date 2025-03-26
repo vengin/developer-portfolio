25e717327e3291f048e0ae4598fc2befd7fbbe96
vengin <gvengin@gmail.com>
2025-03-26 19:59:11 +0100

    Added hobbies

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -4,7 +4,8 @@ import profileImg from '../assets/jpg/profileImg.jpg'
 export const headerData = {
     name: 'Anton Makarevich',
     title: "Lead FPGA/Embedded Developer",
-    desciption: "Accomplished FPGA/Embedded Developer with 17 years of commercial experience in FPGA and embedded systems (software, firmware, hardware), working in teams ranging from 2 to 15 members, both remotely and on-site. My primary area of expertise includes a full cycle of FPGA based product development (prototyping, implementation, debugging, verification, delivery). I am also proficient in C/C++ development for various microprocessors/microcontrollers, embedded systems, drivers and desktop applications. Committed to continuous improvement and problem-solving in complex systems.",
+    description1: "Accomplished FPGA/Embedded Developer with 17 years of commercial experience in FPGA and embedded systems (software, firmware, hardware), working in teams ranging from 2 to 15 members, both remotely and on-site. My primary area of expertise includes a full cycle of FPGA based product development (prototyping, implementation, debugging, verification, delivery). I am also proficient in C/C++ development for various microprocessors/microcontrollers, embedded systems, drivers and desktop applications. Committed to continuous improvement and problem-solving in complex systems.",
+    description2: "My hobbies are 🌍✈️ travelling, 🚴‍♂️ cycling, ⛰️ hiking, 💻 IT, and 🏃‍♂️⏳ adventure racing.",
     image: profileImg,
     resumePdf: resume
 }
