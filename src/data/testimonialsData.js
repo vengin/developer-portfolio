d8506944957239825bda8ca2b12e43f68fb09a46
vengin <gvengin@gmail.com>
2025-04-04 20:27:14 +0200

    Added Harsh's recommendation

--- src/data/testimonialsData.js
+++ src/data/testimonialsData.js
@@ -7,6 +7,7 @@ import boy2 from '../assets/svg/testimonials/boy2.svg'
 import boy3 from '../assets/svg/testimonials/boy3.svg'
 import gautam from '../assets/svg/testimonials/Gautam Kavipurapu.jpg'
 import yaroslav from '../assets/svg/testimonials/Yaroslav Grebenschik.jpg'
+import harsh from '../assets/svg/testimonials/Harsh Nagarkar.jpg'
 
 
 
@@ -14,33 +15,40 @@ export const testimonialsData = [
     {
         id: 1,
         name: 'Gautam Kavipurapu',
-        title: 'CTO and Engineering at Ectron Corp',
+        title: 'CTO and Engineering @ Ectron Corp',
         text: "Anton is one of the best FPGA and Embedded Systems Engineers I have had the pleasure of managing. At Ectron he delivered above and beyond what was required and actively worked with other engineers in the team.",
         image: gautam
     },
     {
-        id: 2,
+      id: 2,
+      name: 'Harsh Nagarkar',
+      title: 'Technical Manager @ Ectron Corp',
+      text: "Anton is exceptionally organized and meticulous in his work, consistently providing valuable insights on projects. Additionally, he is a fantastic team player, always willing to assist others and share his expertise. I highly recommend Anton for his outstanding skills and positive attitude.",
+      image: harsh
+  },
+  {
+        id: 3,
         name: 'Yaroslav Grebenschik',
         title: 'Director of BestSoft FLLC',
         text: "Anton received official recognition for his contributions from 2010 to 2016 for dedication, high professionalism, creative approach to problem-solving, and a significant personal impact on improving the company's efficiency.",
         image: yaroslav
     },
     {
-        id: 3,
+        id: 4,
         name: 'Nermin ',
         title: 'Upwork feedback',
         text: "Anton is a real professional. He's a perfectionist, organised, fast, and is easy to communicate with.",
         image: boy2
       },
     {
-        id: 4,
+        id: 5,
         name: 'Fernando ',
         title: 'Upwork feedback',
         text: "Anton is a competent and very qualified worker, he communicates clearly and fast. Totally recommended.",
         image: boy1
     },
     {
-        id: 5,
+        id: 6,
         name: 'Olajide',
         title: 'Upwork feedback',
         text: 'Very helpful guy, he communicated well and always kept me up to date. I wish I had been in touch with Anton earlier for my previous projects. I didn\'t regret using this freelancer and I would hire again if needed.',
