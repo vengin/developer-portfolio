55766783dd3510a546cc83dad77149367be3b13a
vengin <gvengin@gmail.com>
2025-04-08 07:56:25 +0200

    Added Sergey's recommendation

--- src/data/testimonialsData.js
+++ src/data/testimonialsData.js
@@ -8,6 +8,7 @@ import boy3 from '../assets/svg/testimonials/boy3.svg'
 import gautam from '../assets/svg/testimonials/Gautam Kavipurapu.jpg'
 import yaroslav from '../assets/svg/testimonials/Yaroslav Grebenschik.jpg'
 import harsh from '../assets/svg/testimonials/Harsh Nagarkar.jpg'
+import sergey from '../assets/svg/testimonials/Sergey Ostromukhov.jpg'
 
 
 
@@ -20,35 +21,42 @@ export const testimonialsData = [
         image: gautam
     },
     {
-      id: 2,
-      name: 'Harsh Nagarkar',
-      title: 'Technical Manager @ Ectron Corp',
-      text: "Anton is exceptionally organized and meticulous in his work, consistently providing valuable insights on projects. Additionally, he is a fantastic team player, always willing to assist others and share his expertise. I highly recommend Anton for his outstanding skills and positive attitude.",
-      image: harsh
-  },
-  {
+        id: 2,
+        name: 'Harsh Nagarkar',
+        title: 'Technical Manager @ Ectron Corp',
+        text: "Anton is exceptionally organized and meticulous in his work, consistently providing valuable insights on projects. Additionally, he is a fantastic team player, always willing to assist others and share his expertise. I highly recommend Anton for his outstanding skills and positive attitude.",
+        image: harsh
+    },
+    {
         id: 3,
+        name: 'Sergey Ostromukhov',
+        title: 'Functional Manager for Client Solutions @ PSA, Inc.',
+        text: "I have worked with Anton for about 4 years and can recommend him as a knowledgeable and experienced person, who can not only implement and design programmable logic, but also can successfully interact with customers, clarifying technical requirements, designs, and providing explanations on designs and code. He also has great skills on reverse engineering and preparation of the comprehensive technical documentation for projects. Can successfully work both independently and within a team.",
+        image: sergey
+    },
+    {
+        id: 4,
         name: 'Yaroslav Grebenschik',
         title: 'Director of BestSoft FLLC',
         text: "Anton received official recognition for his contributions from 2010 to 2016 for dedication, high professionalism, creative approach to problem-solving, and a significant personal impact on improving the company's efficiency.",
         image: yaroslav
     },
     {
-        id: 4,
+        id: 5,
         name: 'Nermin ',
         title: 'Upwork feedback',
         text: "Anton is a real professional. He's a perfectionist, organised, fast, and is easy to communicate with.",
         image: boy2
       },
     {
-        id: 5,
+        id: 6,
         name: 'Fernando ',
         title: 'Upwork feedback',
         text: "Anton is a competent and very qualified worker, he communicates clearly and fast. Totally recommended.",
         image: boy1
     },
     {
-        id: 6,
+        id: 7,
         name: 'Olajide',
         title: 'Upwork feedback',
         text: 'Very helpful guy, he communicated well and always kept me up to date. I wish I had been in touch with Anton earlier for my previous projects. I didn\'t regret using this freelancer and I would hire again if needed.',
